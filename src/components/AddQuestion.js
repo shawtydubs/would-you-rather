9cd7c7194c0ebd62fb45ee5c67f67623a476de66
Claire Walters <claire.walters@opower.com>
2018-07-16 10:56:03 -0400

    Add skeleton of AddQuestion component

---
+++ src/components/AddQuestion.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class AddQuestion extends Component {
+    render() {
+        return (
+            <div className="add-question">
+                <h1>Add Question</h1>
+            </div>
+        );
+    };
+};
+
+export default AddQuestion;
