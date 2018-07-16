7369ea0faa0d6e627b869837e297d0873606ec79
Claire Walters <claire.walters@opower.com>
2018-07-15 21:02:12 -0400

    Add _Data and API functions

---
+++ src/utils/api.js
@@ -0,0 +1,24 @@
+import {
+    _getUsers,
+    _getQuestions,
+    _saveQuestion,
+    _saveQuestionAnswer,
+} from './_Data.js';
+
+export const getInitialData = () => {
+    return Promise.all([
+        _getUsers(),
+        _getQuestions(),
+    ]).then(([users, questions]) => ({
+        users,
+        questions,
+    }));
+};
+
+export const saveQuestion = (info) => {
+    return _saveQuestion(info);
+};
+
+export const saveQuestionAnswer = (info) => {
+    return _saveQuestionAnswer(info)
+};
