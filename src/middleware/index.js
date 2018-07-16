ead285b8a9bcd18b391ca6ba50653993471598ae
Claire Walters <claire.walters@opower.com>
2018-07-15 22:50:54 -0400

    Add redux-logger

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,6 +1,8 @@
 import {applyMiddleware} from 'redux';
+import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
 export default applyMiddleware(
-    thunk
+    thunk,
+    logger,
 );
