54acb9edb4d3246a82c11d9ea437d086097972d4
Claire Walters <claire.walters@opower.com>
2018-07-16 11:04:29 -0400

    Add navigation bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import React, { Component, Fragment } from 'react';
-import './App.css';
 import {connect} from 'react-redux';
-import {handleInitialData} from '../actions/shared';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import AddQuestion from './AddQuestion';
 import Home from './Home';
 import Leaderboard from './Leaderboard';
 import Login from './Login';
 import LoadingBar from 'react-redux-loading';
+import Nav from './Nav';
+
+import './App.css';
+
+import {handleInitialData} from '../actions/shared';
 
 class App extends Component {
     componentDidMount() {
@@ -14,15 +20,22 @@ class App extends Component {
 
     render() {
         return (
-            <Fragment>
-                <LoadingBar />
-                <div className="App">
-                    {this.props.loading === true
-                        ? null
-                        : <Leaderboard />
-                    }
-                </div>
-            </Fragment>
+            <Router>
+                <Fragment>
+                    <LoadingBar />
+                    <div className="App">
+                        <Nav />
+                        {this.props.loading === true
+                            ? null
+                            : <div>
+                                <Route path="/" exact component={Home} />
+                                <Route path="/add" component={AddQuestion} />
+                                <Route path="/leaderboard" component={Leaderboard} />
+                              </div>
+                        }
+                    </div>
+                </Fragment>
+            </Router>
         );
     };
 };
---
+++ src/components/Nav.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+export default function Nav () {
+    return (
+        <nav className="nav">
+            <ul>
+                <li>
+                    <NavLink to="/" exact activeClassName="active">
+                        Home
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/add" exact activeClassName="active">
+                        Add Question
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/leaderboard" exact activeClassName="active">
+                        Leaderboard
+                    </NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
+};
