6f260b00b56b77176d7ed3e09d5126d78277a894
Claire Walters <claire.walters@opower.com>
2018-07-16 20:47:06 -0400

    Temporarily add back static authedUser

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -4,6 +4,8 @@ import {receiveQuestions} from './questions';
 import {setAuthedUser} from './authedUser';
 import {showLoading, hideLoading} from 'react-redux-loading';
 
+const AUTHED_USER = 'tylermcginnis';
+
 export function handleInitialData () {
     return (dispatch) => {
         dispatch(showLoading());
@@ -12,6 +14,7 @@ export function handleInitialData () {
             .then(({users, questions}) => {
                 dispatch(receiveUsers(users));
                 dispatch(receiveQuestions(questions));
+                dispatch(setAuthedUser(AUTHED_USER));
                 dispatch(hideLoading());
             });
     };
