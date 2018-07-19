505fdc2ce4d3f13bbf0754dec9d50bbbe4f9fee2
Claire Walters <claire.walters@opower.com>
2018-07-18 20:00:46 -0400

    Allow user to see answered and unanswered questions on homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,26 +3,48 @@ import _ from 'lodash';
 import {connect} from 'react-redux';
 
 class Home extends Component {
+    state = {
+        showAnsweredQuestions: false
+    };
+
+    toggleQuestions = () => {
+        const {showAnsweredQuestions} = this.state;
+
+        this.setState({showAnsweredQuestions: !showAnsweredQuestions})
+    };
+
     render() {
-        const {questions} = this.props;
+        const {questions, user} = this.props;
+        const {showAnsweredQuestions} = this.state;
+
+        const answeredQuestions = _.filter(questions, question => _.includes(_.keys(user.answers), question.id));
+        const unansweredQuestions = _.reject(questions, question => _.includes(_.keys(user.answers), question.id));
+
+        const questionsToShow = showAnsweredQuestions ? answeredQuestions : unansweredQuestions;
+        const titleText = showAnsweredQuestions ? 'Answered Questions' : 'Unanswered Questions';
+        const buttonText = showAnsweredQuestions ? 'Show Unanswered Questions' : 'Show Answered Questions';
 
         return (
             <div className="home">
-                <h1>Would You Rather?</h1>
-                {_.map(questions, question => (
-                    <ul key={question.id}>
-                        <li key={`${question.id}-1`}>{question.optionOne.text}</li>
-                        <li key={`${question.id}-2`}>{question.optionTwo.text}</li>
-                    </ul>
-                ))}
+                <h1>{titleText}</h1>
+                <button className="btn" onClick={this.toggleQuestions}>{buttonText}</button>
+                <div className="question-list">
+                    {_.map(questionsToShow, question => (
+                        <div key={question.id} className="question-panel">
+                            <div className="option one">{question.optionOne.text}</div>
+                            <div className="option two">{question.optionTwo.text}</div>
+                        </div>
+                    ))}
+                </div>
             </div>
         );
     };
 };
 
-const mapStateToProps = ({questions}) => {
+const mapStateToProps = ({authedUser, questions, users}) => {
     return {
-        questions
+        questions,
+        user: users[authedUser]
     }
 };
 
