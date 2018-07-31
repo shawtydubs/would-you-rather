a962fbaeba436f5692e850906e5c3729ea9dad36
Claire Walters <claire.walters@opower.com>
2018-07-31 17:19:59 -0400

    Add question ID to users for new question

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,10 +1,20 @@
-import {ADD_QUESTION_ANSWER} from '../actions/questions';
+import {ADD_QUESTION, ADD_QUESTION_ANSWER} from '../actions/questions';
 import {RECEIVE_USERS} from '../actions/users';
 
 export default function users (state = {}, action) {
     switch (action.type) {
         case RECEIVE_USERS:
             return action.users;
+        case ADD_QUESTION:
+            const {author, id: newId} = action.question;
+
+            return {
+                ...state,
+                [author]: {
+                    ...state[author],
+                    questions: state[author].questions.concat([newId])
+                }
+            }
         case ADD_QUESTION_ANSWER:
             const {authedUser, id, answer} = action;
 
