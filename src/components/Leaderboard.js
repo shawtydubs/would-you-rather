f6d380667551eeb231142abd8ebe258285d11829
Claire Walters <claire.walters@opower.com>
2018-08-01 19:12:17 -0400

    Add required elements and style to leaderboard

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -7,22 +7,59 @@ class Leaderboard extends Component {
         const {users} = this.props;
 
         return (
-            <div className="leaderboard">
+            <div>
                 <h1>Leaderboard</h1>
-                <ul>
-                    {_.map(users, user => {
-                        const score = _.size(user.answers) + _.size(user.questions);
-                        return <li key={user.id}>{user.name}, {score}</li>
-                    })}
-                </ul>
+                <table className="leaderboard">
+                    <tbody>
+                        <tr>
+                            <th>Rank</th>
+                            <th>Name</th>
+                            <th># Asked</th>
+                            <th># Answered</th>
+                            <th>Score</th>
+                        </tr>
+                        {_.map(users, (user, index) => (
+                            <tr key={user.username}>
+                                <td>{index + 1}</td>
+                                <td>
+                                    <div className="leaderboard-user">
+                                        <img src={require(`../images/${user.avatar}`)} className="avatar user-item" alt={`Avatar of ${user.name}`}/>
+                                        {user.name}
+                                    </div>
+                                </td>
+                                <td>{user.asked}</td>
+                                <td>{user.answered}</td>
+                                <td className="leaderboard-score">{user.score}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
         );
     };
 };
 
 const mapStateToProps = ({users}) => {
+    const userScores = [];
+
+    for (let user in users) {
+        userScores.push({
+            username: user,
+            name: users[user].name,
+            avatar: users[user].avatarURL,
+            asked: _.size(users[user].questions),
+            answered: _.size(users[user].answers),
+            score: _.size(users[user].questions) + _.size(users[user].answers)
+        })
+    }
+
+    const sortedUserScores = _(userScores)
+        .sortBy(['score'])
+        .reverse()
+        .valueOf();
+
     return {
-        users
+        users: sortedUserScores
     }
 }
 
